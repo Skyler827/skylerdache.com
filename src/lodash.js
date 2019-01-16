22d38331be1f455f74ea794abb502164ff864e86
Skyler Daché <skyberskyman@gmail.com>
2019-01-16 16:33:06 -0500

    redid everything

--- src/lodash.js
+++
@@ -1,1 +0,0 @@
-module.exports.add = function(a,b) {return a+b};
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
+const fs = require("fs");
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const toTitleCase = require('./src/util').toTitleCase;
+const sitemap = fs.readFileSync(__dirname+"/src/sitemap.html");
 
+const pageNames = ["about", "blog", "dog", "home", "links", "projects", "resume", "skills"]
+const webPagePluginFactory = (pageName) => 
+    new HtmlWebPackPlugin({
+        title: `Skyler Daché - ${toTitleCase(pageName)}`,
+        template: `src/${pageName}/${pageName}.html`,
+        sitemap: sitemap,
+        filename: pageName=="home"? "index.html": pageName+".html",
+        chunks: [pageName]
+    });
 module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js',
-    animation: './src/animation.js'
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebPackPlugin({
-      title: "The Personal Website of Skyler Daché",
-      template: "src/index.html",
-      filename: "index.html",
-      chunks: ['app']
-    }),
-    new HtmlWebPackPlugin({
-      title: "Animation",
-      template: "src/animation.html",
-      filename: "animation.html",
-      chunks: ['animation']
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module:{
-    rules:[
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  mode: 'development'
+    entry: {
+        about: './src/about/about.js',
+        blog: './src/blog/blog.js',
+        dog: './src/dog/dog.js',
+        home: './src/home/home.js',
+        links: './src/links/links.js',
+        projects: './src/projects/projects.js',
+        resume: './src/resume/resume.js',
+        skills: './src/skills/skills.js'
+    },
+    plugins: [new CleanWebpackPlugin(['dist'])].
+        concat(pageNames.map(webPagePluginFactory)),
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[
+            {test: /\.css$/, use: ['style-loader', 'css-loader']}
+        ]
+    },
+    mode: 'development'
 };
