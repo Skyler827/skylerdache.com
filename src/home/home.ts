643e6b02d56408f8e4cb9e6b5200f1d313f8c0d8
Skyler Daché <skyberskyman@gmail.com>
2019-05-22 14:11:05 -0400

    Added const statements to replace let and var

--- src/home/home.ts
+++ src/home/home.ts
@@ -4,30 +4,30 @@ import {
     Scene, PerspectiveCamera, PointLight, BoxGeometry, Mesh,
     Font, TextGeometry, MeshLambertMaterial, WebGLRenderer, AnimationClip, KeyframeTrack,
     NumberKeyframeTrack, Clock, AnimationMixer, LoopPingPong, LoopOnce, AmbientLight, PlaneGeometry, 
-    MeshPhongMaterial, MeshStandardMaterial, Color, HemisphereLight, MeshBasicMaterial, SphereBufferGeometry, BackSide, DoubleSide
+    MeshPhongMaterial, MeshStandardMaterial, Color, HemisphereLight, MeshBasicMaterial, SphereBufferGeometry, BackSide, DoubleSide, Vector3
 } from 'three';
-let stackPositions: string = require("../animation_data/stackPositions.txt").default;
-let stacks: Array<{
+const stackPositions: string = require("../animation_data/stackPositions.txt").default;
+const stacks: Array<{
     name:string, position:{x:number,y:number, z:number},
     startTime: number
 }> = require('../animation_data/stacks.json');
-let fontJSON = require("../../node_modules/three/examples/fonts/helvetiker_regular.typeface.json");
-let scene = new Scene();
-let camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var hemisphereLight = new HemisphereLight( 0xffffbb, 0x080820, 1 );
-let ambientLight = new AmbientLight( 0xaaaaaa ); // soft white light
-let font = new Font(fontJSON);
+const fontJSON = require("../../node_modules/three/examples/fonts/helvetiker_regular.typeface.json");
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const hemisphereLight = new HemisphereLight( 0xffffbb, 0x080820, 1 );
+const ambientLight = new AmbientLight( 0xaaaaaa ); // soft white light
+const font = new Font(fontJSON);
 
-let cubeGeo = new BoxGeometry(1, 0.6, 0.6);
-let greenStuff = new MeshLambertMaterial();
-let greenHex = 0x11aa22
+const cubeGeo = new BoxGeometry(1, 0.6, 0.6);
+const greenStuff = new MeshLambertMaterial();
+const greenHex = 0x11aa22
 greenStuff.color.setHex(greenHex);
-let groundGeo = new PlaneGeometry(4000,4000);
-let groundMaterial = new MeshLambertMaterial();
+const groundGeo = new PlaneGeometry(4000,4000);
+const groundMaterial = new MeshLambertMaterial();
 groundMaterial.color.setHex(0x4a2233);
-let ground = new Mesh(groundGeo, groundMaterial);
+const ground = new Mesh(groundGeo, groundMaterial);
 ground.rotateX(-Math.PI/2);
-ground.translateZ(-10);
+// ground.translateZ(-10);
 scene.add(ground);
 
 // To add a Sky:
@@ -38,38 +38,32 @@ scene.add(ground);
 //var sky = new Mesh( skyGeo, skyMat );
 //scene.add( sky );
 
-let t_max = 3;
-let dt = 0.01;
-let times = Array(Math.ceil(t_max/dt)).fill(0).map((_,i) => dt*i);
-let dropheight = 10;
-let fallInAnimation = ((nodeName:string, x0:number, y0:number, z0:number): AnimationClip => {
-    let x_of_t = (t:number):number => {
-        return x0;
-    };
-    let y_of_t = (t:number):number => {
-        return y0 + dropheight *(1 - Math.pow(t/t_max,2));
-    };
-    let z_of_t = (t:number):number => {
-        return z0;
-    };
+const t_max = 3;
+const dt = 0.01;
+const times = Array(Math.ceil(t_max/dt)).fill(0).map((_,i) => dt*i);
+const dropheight = 10;
+const fallInAnimation = ((nodeName:string, x0:number, y0:number, z0:number): AnimationClip => {
+    const position = (t:number) => new Vector3(
+        x0,
+        y0 + dropheight *(1 - Math.pow(t/t_max,2)),
+        z0
+    );
     return new AnimationClip("Fall In", -1,[
-        new NumberKeyframeTrack(`${nodeName}.position[x]`, times, times.map(x_of_t)),
-        new NumberKeyframeTrack(`${nodeName}.position[y]`, times,times.map(y_of_t)),
-        new NumberKeyframeTrack(`${nodeName}.position[z]`, times, times.map(z_of_t)),
+        new NumberKeyframeTrack(`${nodeName}.position`, times, times.map(position))
     ]);
 });
 
-let lines = stackPositions.split("\n").slice(0,-1);
+const lines = stackPositions.split("\n").slice(0,-1);
 // console.log(lines);
-let cubeCoords = lines.reduce((prevPositions: Array<{x:number, z:number}>, newLine, lineNumber) => 
+const cubeCoords = lines.reduce((prevPositions: Array<{x:number, z:number}>, newLine, lineNumber) =>
     prevPositions.concat(newLine.split('')
     .map((char, colNumber)=>({c:char, x:colNumber, z:lineNumber}))
     .filter(x => x.c === "#")
     .map(o => ({x:o.x, z:o.z}))), []);
 console.log(cubeCoords);
 
-let cubesAndStartTimes: Array<[Mesh, number]> = cubeCoords.map((coordPair, i) => {
-    let c = new Mesh(cubeGeo, greenStuff);
+const cubesAndStartTimes: Array<[Mesh, number]> = cubeCoords.map((coordPair, i) => {
+    const c = new Mesh(cubeGeo, greenStuff);
     c.name = `box${i}`;
     c.position.x = coordPair.x;
     c.position.y = 0;
@@ -85,11 +79,11 @@ let cubesAndStartTimes: Array<[Mesh, number]> = cubeCoords.map((coordPair, i) =>
             bevelSize: 8,
             bevelSegments: 5
         });
-        let textMat = new MeshStandardMaterial();
+        const textMat = new MeshStandardMaterial();
         textMat.color = new Color(0xffffff);
         textMat.depthWrite = false;
         textMat.dithering = true;
-        let text = new Mesh(textGeometry, textMat);
+        const text = new Mesh(textGeometry, textMat);
         text.scale.set(0.0015,0.0015,0.0015);
         text.position.setZ(0.36);
         text.position.setX(-0.35);
@@ -99,16 +93,16 @@ let cubesAndStartTimes: Array<[Mesh, number]> = cubeCoords.map((coordPair, i) =>
 
     return [c, i];
 });
-let mixers = cubesAndStartTimes.map(cubeAndStartTime => {
-    let cube = cubeAndStartTime[0];
-    let startTime = cubeAndStartTime[1];
-    let mixer = new AnimationMixer(cube);
-    let animation = fallInAnimation(cube.name, cube.position.x,cube.position.y, cube.position.z);
-    let animationAction = mixer.clipAction(animation);
+const mixers = cubesAndStartTimes.map(cubeAndStartTime => {
+    const cube = cubeAndStartTime[0];
+    const startTime = cubeAndStartTime[1];
+    const mixer = new AnimationMixer(cube);
+    const animation = fallInAnimation(cube.name, cube.position.x,cube.position.y, cube.position.z);
+    const animationAction = mixer.clipAction(animation);
     animationAction.setLoop(LoopOnce,1);
     animationAction.clampWhenFinished = true;
-    // animationAction.play();
-    // animationAction.startAt(startTime);
+    animationAction.play();
+    animationAction.startAt(startTime);
     return mixer;
 });
 scene.add(...cubesAndStartTimes.map(x=>x[0]));
@@ -118,17 +112,17 @@ scene.add( hemisphereLight );
 camera.position.set(50,50,30);
 camera.lookAt(50,0,0);
 
-let renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth - 10, window.innerHeight - 100 );
 document.body.appendChild( renderer.domElement );
-let clock = new Clock();
-let animate = function () {
+const clock = new Clock();
+const animate = function () {
     let dt = clock.getDelta();
     let t = clock.getElapsedTime();
-    // mixers.forEach(mixer => {
-    //     mixer.update(dt);
-    // });
-    camera.lookAt(50+10*Math.sin(t/2),0,10);
+    mixers.forEach(mixer => {
+        mixer.update(dt);
+    });
+    camera.lookAt(50+3*Math.sin(t/2),3*Math.cos(t/2),10);
     renderer.render( scene, camera );
     requestAnimationFrame( animate );
 };
