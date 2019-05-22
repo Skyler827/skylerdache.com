ac795db99f02b888858efdbe420a612512696640
Skyler Daché <skyberskyman@gmail.com>
2019-05-22 19:11:35 -0400

    Made animation look a bit better

--- src/home/home.ts
+++ src/home/home.ts
@@ -6,48 +6,34 @@ import {
     NumberKeyframeTrack, Clock, AnimationMixer, LoopPingPong, LoopOnce, AmbientLight, PlaneGeometry, 
     MeshPhongMaterial, MeshStandardMaterial, Color, HemisphereLight, MeshBasicMaterial, SphereBufferGeometry, BackSide, DoubleSide, Vector3, VectorKeyframeTrack
 } from 'three';
-import { Z_ASCII } from 'zlib';
 const stackPositions: string = require("../animation_data/stackPositions.txt").default;
-const stacks: Array<{
-    name:string, position:{x:number,y:number, z:number},
-    startTime: number
-}> = require('../animation_data/stacks.json');
-const fontJSON = require("../../node_modules/three/examples/fonts/helvetiker_regular.typeface.json");
+
 const scene = new Scene();
 const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const hemisphereLight = new HemisphereLight( 0xffffbb, 0x080820, 1 );
 const ambientLight = new AmbientLight( 0xaaaaaa ); // soft white light
-const font = new Font(fontJSON);
 
-const cubeGeo = new BoxGeometry(1, 0.6, 0.6);
+const cubeGeo = new BoxGeometry(0.7, 0.5, 0.7);
 const greenStuff = new MeshLambertMaterial();
 const greenHex = 0x11aa22
 greenStuff.color.setHex(greenHex);
-const groundGeo = new PlaneGeometry(4000,4000);
+const groundGeo = new PlaneGeometry(150,30);
 const groundMaterial = new MeshLambertMaterial();
 groundMaterial.color.setHex(0x4a2233);
 const ground = new Mesh(groundGeo, groundMaterial);
 ground.rotateX(-Math.PI/2);
-// ground.translateZ(-10);
-// scene.add(ground);
+ground.translateY(-6);
+ground.translateX(68);
+console.log(ground.position);
 
-// To add a Sky:
-//var skyGeo = new SphereBufferGeometry( 400, 32, 15 );
-//var skyMat = new MeshBasicMaterial();
-//skyMat.color = new Color(0x3344dd);
-//skyMat.side = DoubleSide;
-//var sky = new Mesh( skyGeo, skyMat );
-//scene.add( sky );
+scene.add(ground);
 
 const t_max = 3;
 const dt = 0.1;
 const times = Array(Math.ceil(t_max/dt)).fill(0).map((_,i) => dt*i);
 const dropheight = 50;
 const secondLineCutoff = 8;
-const secondLineDelay = 1.5;
-function validPosition(v:any) {
-    return (v.x!=null) && (v.y!=null) && (v.z!=null);
-}
+const secondLineDelay = 12;
 const fallInAnimation = ((nodeName:string, p0:Vector3): AnimationClip => {
     function position(t:number) {
         return [p0.x, p0.y -dropheight * Math.pow(t/t_max,2), p0.z];
@@ -73,37 +59,35 @@ function distance(a:p,b:p) {
     return Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.z-b.z,2));
 }
 const startingPoint:p = {x:6,z:-4};
+function hash(p:p):number {
+    return Math.pow(p.x + p.z, 100) % 1151;
+}
 cubeCoords.sort((a,b) => {
     if (a.z < secondLineCutoff && b.z >= secondLineCutoff) return -1
     else if (a.z >= secondLineCutoff && b.z < secondLineCutoff) return 1;
+    else if (a.z >= secondLineCutoff && b.z >= secondLineCutoff) return hash(a) - hash(b);
     else return distance(a,startingPoint) - distance(b, startingPoint)
 });
 
-const cubesAndStartTimes: Array<[Mesh, number, AnimationMixer]> = cubeCoords.map((coordPair, i) => {
+const cubesAndStartTimes: Array<[Mesh, AnimationMixer]> = cubeCoords.map((coordPair, i) => {
     const c = new Mesh(cubeGeo, greenStuff);
-    const startTime = Math.sqrt(i) + (coordPair.z > secondLineCutoff ? secondLineDelay:0);
+    const startTime = coordPair.z < secondLineCutoff ? Math.sqrt(i) : secondLineDelay+i/70;
     c.name = `box${i}`;
     c.position.set(coordPair.x, dropheight, coordPair.z);
-    if (!validPosition(c.position)) {
-        console.error("invalid initial position");
-    }
 
 
     const mixer = new AnimationMixer(c);
-    if (!validPosition(c.position)) {
-        console.error("invalid position @107");
-    }
     const animation = fallInAnimation(c.name, c.position);
     const animationAction = mixer.clipAction(animation);
     animationAction.setLoop(LoopOnce,1);
     animationAction.clampWhenFinished = true;
     animationAction.play();
     animationAction.startAt(startTime);
-    return [c, i, mixer];
+    return [c, mixer];
 });
 
 const cubes = cubesAndStartTimes.map(x=>x[0]);
-const mixers = cubesAndStartTimes.map(x=>x[2]);
+const mixers = cubesAndStartTimes.map(x=>x[1]);
 scene.add(...cubes);
 scene.add( ambientLight );
 scene.add( hemisphereLight );
@@ -115,24 +99,14 @@ const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth - 10, window.innerHeight - 100 );
 document.body.appendChild( renderer.domElement );
 const clock = new Clock();
-let nextPrint:number = 0;
-const middleCube = cubesAndStartTimes[Math.floor(cubesAndStartTimes.length/2)][0];
 const animate = function () {
     let dt = clock.getDelta();
     let t = clock.getElapsedTime();
     mixers.forEach((mixer, i) => {
         mixer.update(dt);
-        // if (i==0) console.log(mixer.getRoot().position);
     });
     camera.lookAt(50+3*Math.sin(t/2),3*Math.cos(t/2),10);
-    // camera.lookAt(middleCube.position);
-    if (t > nextPrint) {
-        console.log("middle cube position:");
-        console.log(middleCube.position);
-        console.log("camera position:");
-        console.log(camera.position);
-        nextPrint += 10;
-    }
+    // ground.rotateZ(dt);
     renderer.render( scene, camera );
     requestAnimationFrame( animate );
 };
