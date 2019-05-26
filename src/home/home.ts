c987df2021cfac6c8a9f7b62fbcd099b6aaea5ba
Skyler Daché <skyberskyman@gmail.com>
2019-05-26 16:36:10 -0400

    Worked on using Quaternions for animation
    The following link was really helpful for figuring this stuff out:
    https://quaternions.online/
    (in addition to many others)

--- src/home/home.ts
+++ src/home/home.ts
@@ -5,10 +5,14 @@ import {
     Font, TextGeometry, MeshLambertMaterial, WebGLRenderer, AnimationClip, KeyframeTrack,
     NumberKeyframeTrack, Clock, AnimationMixer, LoopPingPong, LoopOnce, AmbientLight, PlaneGeometry, 
     MeshPhongMaterial, MeshStandardMaterial, Color, HemisphereLight, MeshBasicMaterial, SphereBufferGeometry, BackSide, 
-    DoubleSide, Vector3, VectorKeyframeTrack, QuaternionKeyframeTrack, Side
+    DoubleSide, Vector3, VectorKeyframeTrack, QuaternionKeyframeTrack, Side, Quaternion
 } from 'three';
-const stackPositions: string = require("../animation_data/stackPositions.txt").default;
 
+const sin = Math.sin;
+const pow = Math.pow;
+const sqrt = Math.sqrt;
+
+const stackPositions: string = require("../animation_data/stackPositions.txt").default;
 const scene = new Scene();
 const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const hemisphereLight = new HemisphereLight( 0xffffbb, 0x080820, 1 );
@@ -37,7 +41,7 @@ const secondLineCutoff = 8;
 const secondLineDelay = 12;
 const fallInAnimation = ((p0:Vector3): AnimationClip => {
     function position(t:number) {
-        return [p0.x, p0.y -dropheight * Math.pow(t/t_max,2), p0.z];
+        return [p0.x, p0.y -dropheight * pow(t/t_max,2), p0.z];
     }
     return new AnimationClip("Fall In", -1,[
         new VectorKeyframeTrack(`.position`, times, times.flatMap(position))
@@ -57,11 +61,11 @@ const cubeCoords = lines.flatMap((newLine, lineNumber) =>
 
 type p = {x:number, z:number};
 function distance(a:p,b:p) {
-    return Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.z-b.z,2));
+    return sqrt(pow(a.x-b.x,2)+pow(a.z-b.z,2));
 }
 const startingPoint:p = {x:6,z:-4};
 function hash(p:p):number {
-    return Math.pow(p.x + p.z, 100) % 1151;
+    return pow(p.x + p.z, 100) % 1151;
 }
 cubeCoords.sort((a,b) => {
     if (a.z < secondLineCutoff && b.z >= secondLineCutoff) return -1
@@ -92,7 +96,13 @@ scene.add( hemisphereLight );
 
 camera.position.set(50,50,50);
 camera.lookAt(50,0,0);
-
+function quaternionArray(x:number, y:number, z:number):number[] {
+    let quat = new Quaternion().setFromUnitVectors(
+        new Vector3(0,0,-1),
+        new Vector3(x,y,z).normalize());
+    console.log(quat);
+    return quat.toArray();
+}
 const cameraMaxTime = 8*Math.PI;
 const cameraDt = 0.1;
 const cameraTimes = Array(Math.ceil(cameraMaxTime/cameraDt)).fill(0).map((_,i) => cameraDt*i);
@@ -100,29 +110,29 @@ const cameraMixer = new AnimationMixer(camera);
 const cameraPan = new AnimationClip("camera_pan",-1, [
     new VectorKeyframeTrack(".position", [0,3,6,9,12,15.71], [
         //x, y, z:
-        3,  4, 4.5,
-        3,  4, 4.5,
-        3.5,3, 4.7,
-        50, 9, 30,
-        40, 20, 17,
+        3,  4, 7,
+        3,  4, 7,
+        3.5,4, 7,
+        2,  9, 9,
+        15, 20, 17,
         50, 40, 30
     ]),
-    new QuaternionKeyframeTrack(".quaternion", [0,3,6,9,12,15.71], [
-        //inclination, horizontal, twist, leftover
-        0.35,  0,   0, 0.9,
-        0.15,  0.3, 0, 0.95,
-        -0.05, 0.3, 0, 0.7,
-        -0.1,  0.1, 0, 0.8,
-        -0.2, -0.1, 0, 0.9,
-        -0.45, 0,   0, 0.87
-    ])
+    new QuaternionKeyframeTrack(".quaternion", [0,3,6,9,12,15.71],[
+        quaternionArray(0,-0.2,-0.9),
+        quaternionArray(0,-0.3,-0.9),
+        quaternionArray(0.7,-0.1,-0.2),
+        quaternionArray(-0.3,0,-0.6),
+        quaternionArray(0,-0.4,-0.8),
+        quaternionArray(0,-0.4,-0.8)
+    ].flat())
 ]);
 const cameraPanLoop = new AnimationClip("camera_pan_loop", -1, [
     new VectorKeyframeTrack(".position", cameraTimes, cameraTimes.flatMap(t => 
-        [50,50+10*Math.sin(3*t),50]
+        // x, y, z:
+        [50, 50+5*sin(3*t), 50]
     )),
     new QuaternionKeyframeTrack(".quaternion", cameraTimes, cameraTimes.flatMap(t => 
-        [-0.42+0.05*Math.sign(t),0,0,0.9]
+        [0.373+0.02*sin(t),0.373+0.02*sin(t),-0.601-0.02*sin(t),0.601+0.02*sin(t)]
     ))
 ]);
 const cameraPanAction = cameraMixer.clipAction(cameraPan);
