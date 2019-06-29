17379e1da3783b551f5e1e80be71e6289ddc8ee3
Skyler Daché <skyberskyman@gmail.com>
2019-06-29 10:27:45 -0400

    Added code for handling skill snad projects on seperate pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,33 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const toTitleCase = require('./src/util').toTitleCase;
-const sitemap = fs.readFileSync(__dirname+"/src/sitemap.html");
+const sitemap = fs.readFileSync(path.join(__dirname, "src","sitemap.html"));
 
-const pageNames = ["hire", "home", "links", "projects", "resume", "skills"]
-const webPagePluginFactory = (pageName) => 
+const pageNames = fs
+    .readdirSync(path.join(__dirname,"src"), {withFileTypes:true})
+    .filter(d => d.isDirectory())
+    .filter(d => d.name !== "animation_data")
+    .map(d => d.name);
+const skills = fs
+    .readdirSync(path.join(__dirname,"src","skills"), {withFileTypes:true})
+    .filter(d => d.isDirectory())
+    .map(d => d.name);
+const projects = fs
+    .readdirSync(path.join(__dirname,"src","projects"), {withFileTypes:true})
+    .filter(d => d.isDirectory())
+    .map(d => d.name);
+const webPagePluginFactory = (pageName, directory) =>
     new HtmlWebPackPlugin({
         title: `Skyler Daché - ${toTitleCase(pageName)}`,
-        template: `src/${pageName}/${pageName}.html`,
+        template: directory ?
+            path.join('src',directory,pageName,pageName+'.html'):
+            path.join('src',pageName,pageName+'.html'),
         sitemap: sitemap,
-        filename: pageName=="home"? "index.html": pageName+".html",
+        filename: pageName=="home"?
+            "index.html":
+            directory?
+                path.join(directory,pageName+".html"):
+                pageName+".html",
         chunks: [pageName]
     });
 module.exports = {
@@ -24,8 +42,10 @@ module.exports = {
         skills: './src/skills/skills.ts'
     },
     devtool: 'inline-source-map',
-    plugins: [new CleanWebpackPlugin(['dist'])].
-        concat(pageNames.map(webPagePluginFactory)),
+    plugins: [new CleanWebpackPlugin(['dist'])]
+        .concat(pageNames.map(s => webPagePluginFactory(s)))
+        .concat(skills.map(s => webPagePluginFactory(s, 'skills')))
+        .concat(projects.map(s => webPagePluginFactory(s, 'projects'))),
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
